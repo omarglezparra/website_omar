e088a2dfc36ccbabd29d4c1b298dd6015ac0fa08
omarglezparra <65732945+omarglezparra@users.noreply.github.com>
2026-05-23 15:43:10 +0100

    Create Blog.jsx

---
+++ src/components/Blog.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Blog = () => {
+  return (
+    <section id="blog">
+      <h2>Blog</h2>
+
+      <p>
+        Articles about AI, quantum computing,
+        and scalable systems.
+      </p>
+    </section>
+  );
+};
