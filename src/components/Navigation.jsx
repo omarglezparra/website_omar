141a44af1151b7686c4e5f82d379ead490bc6250
omarglezparra <65732945+omarglezparra@users.noreply.github.com>
2026-05-23 20:05:46 +0100

    Refactor Navigation component structure and links

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,58 +1,18 @@
 import React from "react";
 
-export const Navigation = ({ currentPage, cartItemCount }) => {
-  const primaryLinks = [
-    { href: "#/", label: "Home", page: "home" },
-    { href: "#/products", label: "Products", page: "products" },
-  ];
-
-  const showCheckoutLink = currentPage !== "home" || cartItemCount > 0;
-
+export const Navigation = () => {
   return (
-    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
-      <div className="container">
-        <div className="navbar-header">
-          <button
-            type="button"
-            className="navbar-toggle collapsed"
-            data-toggle="collapse"
-            data-target="#bs-example-navbar-collapse-1"
-          >
-            {" "}
-            <span className="sr-only">Toggle navigation</span>{" "}
-            <span className="icon-bar"></span>{" "}
-            <span className="icon-bar"></span>{" "}
-            <span className="icon-bar"></span>{" "}
-          </button>
-          <a className="navbar-brand page-scroll" href="#page-top">
-            Frankie's Bakery
-          </a>{" "}
-        </div>
-
-        <div
-          className="collapse navbar-collapse"
-          id="bs-example-navbar-collapse-1"
-        >
-          <ul className="nav navbar-nav navbar-right">
-            {primaryLinks.map((link) => (
-              <li
-                key={link.page}
-                className={currentPage === link.page ? "active" : ""}
-              >
-                <a href={link.href}>{link.label}</a>
-              </li>
-            ))}
-            {showCheckoutLink ? (
-              <li className={currentPage === "checkout" ? "active" : ""}>
-                <a href="#/checkout">
-                  Checkout
-                  {cartItemCount > 0 ? ` (${cartItemCount})` : ""}
-                </a>
-              </li>
-            ) : null}
-          </ul>
-        </div>
+    <nav className="navigation">
+      <div className="nav-logo">
+        Omar Gonzalez
       </div>
+
+      <ul className="nav-links">
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#blog">Blog</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
     </nav>
   );
 };
